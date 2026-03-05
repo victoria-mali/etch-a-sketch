59b7eb31ff1f27d6c7f8eef78f0a52d81b4d5602
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-03-05 16:50:48 +0000

    Add create new grid function

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 
 const container = document.querySelector(".container");
-
-
-for (let i = 0; i < 256; i++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("square");
-    width = 800 / 16;
-    squareDiv.style.width = width + "px";
-    container.appendChild(squareDiv);
-};
-
-const divs = document.querySelectorAll(".square");
-
-divs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-    div.classList.add("hover");
-});
-}) 
 let squaresPerSide;
 
 function createGrid(grid, squaresPerSide) {
     for (let i = 0; i < grid; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
-    width = 800 / squaresPerSide;
+    width = 650 / squaresPerSide;
     squareDiv.style.width = width + "px";
     container.appendChild(squareDiv);
-};
 }
+}
+
+createGrid(256, 16);
 
 
 function updateGrid(squaresPerSide) {
-    squaresPerSide = parseFloat(prompt("Enter a number for squares per side up to 100"));
-    console.log(squaresPerSide);
+    const divs = document.querySelectorAll(".square");
     divs.forEach(div => div.remove());
-    let grid = squaresPerSide * squaresPerSide;
-    createGrid(grid, squaresPerSide);
+    squaresPerSide = parseFloat(prompt("Enter a number for squares per side up to 100"));
+
+    if (squaresPerSide > 100) {
+        alert("Error! Input a number from 1 to 100");
+    } else {
+        let grid = squaresPerSide * squaresPerSide;
+        createGrid(grid, squaresPerSide);
+    }
 }
 
 
 const btnChange = document.querySelector(".btn-change");
-
 btnChange.addEventListener("click", updateGrid);
+
+
+const divs = document.querySelectorAll(".square");
+divs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+    div.classList.add("hover");
+});
+}) 
 
