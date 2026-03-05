62b6b94cabc59dd7cfcd24cccdc492f6ca30df85
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-03-05 12:43:25 +0000

    Make new grid when entering a prompt For now the grids build one after the another, not erasing the previous one

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
+    width = 800 / 16;
+    squareDiv.style.width = width + "px";
     container.appendChild(squareDiv);
 };
 
@@ -15,23 +17,25 @@ divs.forEach((div) => {
     div.classList.add("hover");
 });
 }) 
+let squaresPerSide;
 
-
-function createGrid(grid) {
+function createGrid(grid, squaresPerSide) {
     for (let i = 0; i < grid; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
+    width = 800 / squaresPerSide;
+    squareDiv.style.width = width + "px";
     container.appendChild(squareDiv);
 };
 }
 
-let squaresPerSide;
 
 function updateGrid(squaresPerSide) {
     squaresPerSide = parseFloat(prompt("Enter a number for squares per side up to 100"));
+    console.log(squaresPerSide);
     divs.forEach(div => div.remove());
     let grid = squaresPerSide * squaresPerSide;
-    createGrid(grid);
+    createGrid(grid, squaresPerSide);
 }
 
 
