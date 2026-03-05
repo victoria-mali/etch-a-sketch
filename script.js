a3dea692b31af487b673b6ee6d69fd000924d52f
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-03-05 12:22:12 +0000

    Start to work on creating a new grid

--- script.js
+++ script.js
@@ -14,9 +14,28 @@ divs.forEach((div) => {
     div.addEventListener("mouseover", () => {
     div.classList.add("hover");
 });
-})
+}) 
 
 
-/* div.addEventListener("mouseover", () => {
-    div.classList.add("hover");
-}); */
+function createGrid(grid) {
+    for (let i = 0; i < grid; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    container.appendChild(squareDiv);
+};
+}
+
+let squaresPerSide;
+
+function updateGrid(squaresPerSide) {
+    squaresPerSide = parseFloat(prompt("Enter a number for squares per side up to 100"));
+    divs.forEach(div => div.remove());
+    let grid = squaresPerSide * squaresPerSide;
+    createGrid(grid);
+}
+
+
+const btnChange = document.querySelector(".btn-change");
+
+btnChange.addEventListener("click", updateGrid);
+
